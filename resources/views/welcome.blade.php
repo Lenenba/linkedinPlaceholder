21649f0debffc1db131ac261089352e8ba765369
lenenba <bilitikjulesroger@yahoo.fr>
2020-11-09 12:34:34 -0500

    finalisation de la barre de naviguation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,69 +21,15 @@
     </head>
     <body>
         <div id="app">
-            <header class="h-14 w-full flex max-w-6xl relative mx-auto px-6">
-               <div class="flex">
-                    <img src="{{ asset('images/linkedin.png') }}" alt="" class="h-8 w-8 my-2 mr-2">
-                    <div class="flex items-center bg-gray-200 my-2 rounded h-8 w-64 mr-40">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 text-gray-600 mx-4 focus:none">
-                              <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                            </svg>
-                            <input class="w-full roundedh-8 bg-gray-200" type="text" placeholder="Recherche">
-                    </div>
-                    <nav>
-                        <ul class="flex">
-                            <a href="/">
-                                <li class="flex-col justify-items-auto border-b-2 px-4 border-gray-700">  
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 text-gray-700 focus ml-2 -mb-1">
-                                        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-                                    </svg>
-                                    <span class="text-xs text-gray-700">Accueil</span>
-                                </li>
-                            </a>
-                            <a href="/">
-                                <li class="flex-col justify-items-auto px-4 border-gray-700">  
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 text-gray-700 focus ml-2 -mb-1">
-                                        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-                                    </svg>
-                                    <span class="text-xs text-gray-700">Réseau</span>
-                                </li>
-                            </a>
-                            <a href="/">
-                                <li class="flex-col justify-items-auto px-4 border-gray-700">  
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 text-gray-700 focus ml-2 -mb-1">
-                                        <path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd" />
-                                        <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
-                                    </svg>
-                                    <span class="text-xs text-gray-700">Emplois</span>
-                                </li>
-                            </a>
-                            <a href="/">
-                                <li class="flex-col justify-items-auto px-4 border-gray-700">  
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 text-gray-700 focus ml-6 -mb-1">
-                                       <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
-                                    </svg>
-                                    <span class="text-xs text-gray-700">Messagerie</span>
-                                </li>
-                            </a>
-                            <a href="/">
-                                <li class="flex-col justify-items-auto px-4 border-gray-700">  
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 text-gray-700 focus ml-6 -mb-1">
-                                        <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
-                                    </svg>
-                                    <span class="text-xs text-gray-700">Notifications</span>
-                                </li>
-                            </a>
-                        </ul>
-                    </nav>
-               </div>
-            </header>
-            <div class="section">
-                <main class="bg-gray-100 max-h-screen-xl  py-4 ">
+            @include('partials._header')
+            <main class="bg-gray-100">
+                <div class="h-14 w-full flex max-w-6xl relative mx-auto px-6 bg-gray-100">
+                    test
                     @yield('content')
-                </main>
-            </div>
+                </div>
+            </main>
             <footer>
-            <main class="bg-gray-100 px-8 py-4 h-64">
+            <main class="bg-gray-100">
                     @yield('footer')
                 </main>
             </footer>
